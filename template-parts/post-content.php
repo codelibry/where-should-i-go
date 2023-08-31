5f1428e7a1de1e2ff9c39572db213e25c5e4794b
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2023-08-31 17:20:03 +0300

    Single post page creation

---
+++ template-parts/post-content.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Template to show post content of single post template
+ */
+
+$title = get_the_title();
+$text = get_field('hero_text');
+$img = get_the_post_thumbnail();
+$content = get_the_content();
+
+?>
+
+<section class="hero">
+    <div class="container">
+        <div class="row">
+            <div class="hero__content col-lg-8">
+                <?php if($title): ?>
+                    <h1 class="hero__title"><?php echo $title; ?></h1>
+                <?php endif; ?>
+                <?php if($text): ?>
+                    <div class="hero__text"><?php echo $text; ?></div>
+                <?php endif; ?>
+            </div>
+            <?php if ($img) : ?>
+                <div class="col-lg-4 hero__img-column">
+                    <div class="hero__img-wrapper">
+                        <?php echo $img; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
+
+
+<?php if($content): ?>
+
+    <section class="termsText">
+        <div class="container">
+            <div class="termsText__content content-block">
+                <?php echo $content; ?>
+            </div>
+        </div>
+    </section>
+
+<?php endif; ?>
