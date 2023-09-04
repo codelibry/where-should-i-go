55effbe1dd593088af9bbee7966dd84ec96b5cf7
KornienkoIvan <kornienkoivan462@gmail.com>
2023-09-04 14:32:02 +0300

    slider test

--- js/parts/slider.js
+++ js/parts/slider.js
@@ -172,7 +172,6 @@ function basicSliders(){
                 var titleWidth = $(this).find('.favorities__sliderItem__content').outerWidth();
                 var slideWIdth = $(this).find('.favorities__sliderItem__head').outerWidth();
                 $(this).find('.favorities__sliderItem__image img').css('width', slideWIdth - titleWidth - 20);
-                //alert(slideWIdth - titleWidth);
             });
         }
     })
