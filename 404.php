79fea44204c2164f785d8bdba2c016cb4244a13c
KornienkoIvan <ivan.k@flooow.agency>
2023-09-21 16:53:14 +0300

    fix 404 page text

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
         <div class="container">
                 <div class="errorBlock__content">
                         <h1 class="errorBlock__title"><?php _e('404','wsig');?></h1>
-                        <h4 class="errorBlock__subtitle"><?php _e("Oops! <br>Looks like you got lost..","wsig");?></h4>
+                        <h4 class="errorBlock__subtitle"><?php _e("Oops! <br>Looks like you got lost.","wsig");?></h4>
                         <div class="errorBlock__button"><a href="<?php echo get_home_url(  ) ?>"><?php _e('Back to home','wsig'); ?></a></div>
                 </div>
         </div>
