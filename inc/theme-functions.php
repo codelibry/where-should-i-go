074e322be477789a48709676e78422a28a7c6433
KornienkoIvan <ivan.k@codelibry.com>
2024-08-19 02:55:53 +0300

    disable comments

--- inc/theme-functions.php
+++ inc/theme-functions.php
@@ -142,3 +142,38 @@
     }
     
     add_filter('posts_where', 'posts_by_title', 10, 2);
+
+
+//Remove Comments 
+add_action('admin_init', function () {
+    global $pagenow;
+     
+    if ($pagenow === 'edit-comments.php') {
+        wp_safe_redirect(admin_url());
+        exit;
+    }
+ 
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+ 
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+});
+ 
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+ 
+add_filter('comments_array', '__return_empty_array', 10, 2);
+ 
+add_action('admin_menu', function () {
+    remove_menu_page('edit-comments.php');
+});
+ 
+add_action('init', function () {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+});
